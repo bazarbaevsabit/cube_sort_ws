e686d89bf2f701396da9a97afa173cff25ef420c
bazarbaevsabit <bazarbaevsabit@gmail.com>
2026-04-24 11:36:42 +0600

    изменил структуру проекта

--- run.sh
+++ run.sh
@@ -7,16 +7,17 @@ docker run -it --rm \
     -e DISPLAY=$DISPLAY \
     -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    -v /home/user/color-follower-ros2/src:/home/work \
+    -v /home/user/my_project/color-follower-ros2/src:/home/work \
     --device /dev/dri:/dev/dri \
     -p 2222:22 \
     --name gazebo_test \
     ros2_full:v1 \
     /bin/bash -c "
-            cd /home/work
-            cd project_w/src
+            mkdir -p /home/work/prg_w/src
+            cd /home/work/prg_w/src
             ros2 pkg create --build-type ament_cmake mobile
-            cd /home/work/project_w
+            cp -r /home/work/mobile/src/* /home/work/prg_w/src/mobile/
+            cd /home/work/prg_w/src
             colcon build
             source install/setup.bash
             exec /bin/bash
