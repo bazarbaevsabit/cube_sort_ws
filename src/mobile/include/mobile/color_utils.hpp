9f887b0d176bb346b8dc6162304fb17c5417075e
bazarbaevsabit <bazarbaevsabit@gmail.com>
2026-04-25 11:00:08 +0600

    Fix directory layout, add unit tests and CI smoke test

--- src/mobile/include/mobile/color_utils.hpp
+++ src/mobile/include/mobile/color_utils.hpp
@@ -1,4 +1,3 @@
-// src/mobile/include/mobile/color_utils.hpp
 #ifndef MOBILE__COLOR_UTILS_HPP_
 #define MOBILE__COLOR_UTILS_HPP_
 
@@ -6,4 +5,4 @@
 
 std::string getColor(float h, float s, float v);
 
-#endif  // MOBILE__COLOR_UTILS_HPP_6
+#endif  // MOBILE__COLOR_UTILS_HPP_
